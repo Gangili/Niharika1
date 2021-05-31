d3e68f165894da7c623d1fd54415fbff543b8cfd
Gangili <munni.niharika@gmail.com>
2021-05-31 01:43:15 -0400

    Added the message in helloclass

--- helloworld/src/helloworld/hello.java
+++ helloworld/src/helloworld/hello.java
@@ -2,8 +2,10 @@ package helloworld;
 
 public class hello {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		System.out.println("hello");
+		System.out.println("How are you");
 
 	}
 
